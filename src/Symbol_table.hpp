09c31c8069579d2a4a9ab43c331ecd5204e7100f
pku2100012821 <yinzhehui@stu.pku.edu.cn>
2024-12-25 13:58:26 +0000

    111

--- src/Symbol_table.hpp
+++ src/Symbol_table.hpp
@@ -2,139 +2,113 @@
 #include <string>
 #include <memory>
 #include <unordered_map>
-#include <stack>
-#include <sstream>
+#include <vector>
+#include <cassert>
 
-using std::string;
-using std::shared_ptr;
-using std::unordered_map;
+//inline避免重复定义问题
 
 // 符号表中符号的类型
 enum symbol_type
 {
-    SYM_TYPE_CONST, // 常量
-    SYM_TYPE_VAR,   // 变量
-    SYM_TYPE_UND    // 符号不存在
+  SYM_TYPE_CONST, // 常量
+  SYM_TYPE_VAR,   // 变量
+  SYM_TYPE_UND    // 符号不存在
 };
 
 // 符号表中符号的值
 struct symbol_value
 {
-    symbol_type type; // 符号的类型
-    int value;        // 符号的值
+  symbol_type type; // 符号的类型
+  int value;        // 符号的值
 };
 
-// 符号表类型
-using symbol_table_t = unordered_map<string, shared_ptr<symbol_value>>;
+// 作用域形成了一个树形结构, 而每个时刻需要保存的数个作用域形成了树上从根到某个节点的
+// 一条链. 使用一个 std::vector 来维护这个链, 形成一个栈
 
-namespace SymbolTableNamespace
-{
-    // 作用域栈
-    inline std::stack<symbol_table_t> scope_stack;
-    inline int scope_counter = 0;
+// 作用域计数, 已经使用了了多少个符号表
+static int symbol_table_cnt = 0;
 
-    // 进入新的作用域
-    inline void enter_code_block()
-    {
-        scope_stack.push(symbol_table_t());
-        scope_counter++;
-    }
+// 作用域栈, 内容物为 std::pair<符号表标号, 符号表>
+typedef std::unordered_map<std::string, std::shared_ptr<symbol_value> > symbol_table_t;
+typedef std::vector<std::pair<int, symbol_table_t*> > symbol_table_stack_t;
+static symbol_table_stack_t symbol_table_stack;
 
-    // 离开当前作用域
-    inline void exit_code_block()
-    {
-        if (!scope_stack.empty())
-        {
-            scope_stack.pop();
-        }
-    }
-
-    // 返回当前符号表的表号(作用域号), 格式形如 "SYM_TABLE_233_"
-    inline std::string current_code_block()
-    {
-        std::ostringstream oss;
-        oss << "SYM_TABLE_" << scope_counter << "_";
-        return oss.str();
-    }
-
-    // 插入符号定义
-    inline void insert_symbol(const string &symbol, symbol_type type, int value)
-    {
-        if (!scope_stack.empty())
-        {
-            auto &current_scope = scope_stack.top();
-            auto symval = std::make_shared<symbol_value>(symbol_value{ type, value });
-            current_scope[symbol] = symval;
-        }
-    }
-
-    // 确认符号定义是否存在, 若存在返回1, 否则返回0
-    inline int exist_symbol(const string &symbol)
-    {
-        std::stack<symbol_table_t> temp_stack = scope_stack;
-        while (!temp_stack.empty())
-        {
-            auto &current_scope = temp_stack.top();
-            if (current_scope.find(symbol) != current_scope.end())
-            {
-                return 1;
-            }
-            temp_stack.pop();
-        }
-        return 0;
-    }
-
-    // 查询符号定义, 返回该符号所在符号表表号和指向这个符号的值的指针.
-    // 符号表表号格式形如 "SYM_TABLE_233_"
-    // 若符号不存在, 返回的表号为-1, symbol_type为UND
-    inline std::pair<std::string, std::shared_ptr<const symbol_value>> query_symbol(const string &symbol)
-    {
-        std::stack<symbol_table_t> temp_stack = scope_stack;
-        int temp_counter = scope_counter;
-        while (!temp_stack.empty())
-        {
-            auto &current_scope = temp_stack.top();
-            auto it = current_scope.find(symbol);
-            if (it != current_scope.end())
-            {
-                std::ostringstream oss;
-                oss << "SYM_TABLE_" << temp_counter << "_";
-                return {oss.str(), it->second};
-            }
-            temp_stack.pop();
-            temp_counter--;
-        }
-        return {"-1", std::make_shared<symbol_value>(symbol_value{SYM_TYPE_UND, 0})};
-    }
-}
-
-// 全局函数接口
+// 进入新的作用域
 inline void enter_code_block()
 {
-    SymbolTableNamespace::enter_code_block();
+  symbol_table_t* ptr = new symbol_table_t();
+  symbol_table_stack.push_back(std::make_pair(symbol_table_cnt, ptr));
+  symbol_table_cnt++;
 }
 
+// 离开当前作用域
 inline void exit_code_block()
 {
-    SymbolTableNamespace::exit_code_block();
+  delete symbol_table_stack.back().second;
+  symbol_table_stack.pop_back();
 }
 
+// 返回当前作用域的标号, 格式形如 "SYM_TABLE_233"
 inline std::string current_code_block()
 {
-    return SymbolTableNamespace::current_code_block();
+  return "SYM_TABLE_" + std::to_string(symbol_table_stack.back().first) + "_";
 }
 
-inline void insert_symbol(const string &symbol, symbol_type type, int value)
+// 在符号表中寻找符号, 返回其所在符号表的 标号 和其本身的 iterator
+inline std::pair<int, symbol_table_t::iterator> find_iter(const std::string &symbol)
 {
-    SymbolTableNamespace::insert_symbol(symbol, type, value);
+  for(auto rit = symbol_table_stack.rbegin(); rit != symbol_table_stack.rend(); ++rit)
+  {
+    auto it = rit->second->find(symbol);
+    if(it == rit->second->end()) continue;
+    return std::make_pair(rit->first, it);
+  }
+  // 没找到
+  return std::make_pair(-1, symbol_table_stack.back().second->end());
 }
 
-inline int exist_symbol(const string &symbol)
+// 插入符号定义
+inline void insert_symbol(const std::string &symbol, symbol_type type, int value)
 {
-    return SymbolTableNamespace::exist_symbol(symbol);
+  int symtid;
+  symbol_table_t::iterator it;
+  std::tie(symtid, it) = find_iter(symbol);
+
+  // 插入该符号
+  auto symval = std::make_shared<symbol_value>();
+  symval->type = type;
+  symval->value = value;
+  (*(symbol_table_stack.back().second))[symbol] = symval;
 }
 
-inline std::pair<std::string, std::shared_ptr<const symbol_value>> query_symbol(const string &symbol)
+// 确认符号定义是否存在, 若存在返回1, 否则返回0
+inline int exist_symbol(const std::string &symbol)
 {
-    return SymbolTableNamespace::query_symbol(symbol);
+  int symtid;
+  symbol_table_t::iterator it;
+  std::tie(symtid, it) = find_iter(symbol);
+  return (symtid != -1);
+}
+
+// 查询符号定义, 返回该符号所在符号表表号和指向这个符号的值的指针.
+// 若符号不存在, 返回的表号为-1, symbol_type为UND
+inline std::pair<std::string, std::shared_ptr<const symbol_value>> query_symbol(const std::string &symbol)
+{
+  int symtid;
+  symbol_table_t::iterator it;
+  std::tie(symtid, it) = find_iter(symbol);
+
+  std::string str = "SYM_TABLE_" + std::to_string(symtid) + "_";
+
+  // 若符号不存在
+  if(symtid == -1)
+  {
+    auto symval = std::make_shared<symbol_value>();
+    symval->type = SYM_TYPE_UND;
+    symval->value = -1;
+    return std::make_pair(str, symval);
+  }
+
+  // 符号存在, 返回其value
+  return std::make_pair(str, it->second);
 }
