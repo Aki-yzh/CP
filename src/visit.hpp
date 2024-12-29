d694fd769aadc2552330bdb13d3d064f4f26b01f
pku2100012821 <yinzhehui@stu.pku.edu.cn>
2024-12-29 14:23:35 +0000

    lv 8 koopa done risv give up

--- src/visit.hpp
+++ src/visit.hpp
@@ -28,3 +28,11 @@ void Visit(const koopa_raw_return_t &ret);
 void Visit(const koopa_raw_branch_t &branch);
 // 访问 jump 指令
 void Visit(const koopa_raw_jump_t &jump);
+
+
+
+// 访问 call 指令
+void Visit(const koopa_raw_call_t &call, const koopa_raw_value_t &value);
+
+// 访问 global alloc 指令
+void Visit(const koopa_raw_global_alloc_t &global_alloc, const koopa_raw_value_t &value);
